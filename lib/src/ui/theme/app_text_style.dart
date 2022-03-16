74ae5c0c399356ebdf76f62c4f0cb3e12b83c33a
Michael Jordan <mjordan@quiverware.com>
2022-03-16 17:15:18 -0600

    About page refactoring and cleanup

--- lib/src/ui/theme/app_text_style.dart
+++ lib/src/ui/theme/app_text_style.dart
@@ -3,6 +3,7 @@
 //
 
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class AppTextStyle {
   AppTextStyle._();
@@ -56,4 +57,44 @@ class AppTextStyle {
         fontSize: 12,
         fontWeight: FontWeight.normal,
       );
+}
+
+class MarkdownStyle {
+  MarkdownStyle._();
+
+  static const TextStyle headerStyle1 = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Lato',
+    fontSize: 32,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+    // letterSpacing: 1,
+    // height: 1
+  );
+
+  static const TextStyle headerStyle2 = TextStyle(
+    color: Colors.black87,
+    fontFamily: 'Lato',
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  );
+
+  static final MarkdownStyleSheet main = MarkdownStyleSheet(
+    blockSpacing: 21,
+    h1: headerStyle1.copyWith(color: Colors.black87),
+    h2: headerStyle2,
+    h3: headerStyle1.copyWith(
+        color: Colors.black87, fontSize: 24, fontWeight: FontWeight.w600),
+    h4: headerStyle1.copyWith(
+        color: Colors.black87, fontSize: 16, fontWeight: FontWeight.w600),
+    p: headerStyle1.copyWith(
+      color: Colors.black,
+      fontFamily: 'Lato',
+      fontSize: 18,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal,
+      height: 1.6,
+    ),
+  );
 }
